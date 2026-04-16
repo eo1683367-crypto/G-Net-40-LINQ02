a27ec403f594e514ac644c16002b7c0c353f43aa
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-04-16 02:24:13 +0200

    Question 02

--- G-Net-40-LINQ02/Program.cs
+++ G-Net-40-LINQ02/Program.cs
@@ -15,3 +15,16 @@
 #endregion
 
 
+#region Question 02
+
+// 2. show page 2 of products, with page size = 5
+
+//var Result = ProductList.Skip(5).Take(5);
+
+//foreach (var n in Result)
+//{
+//    Console.WriteLine(n);
+//}
+#endregion
+
+
