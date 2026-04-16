687fc518680231b90cbbbd211361e5324ac784fb
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-04-16 02:22:21 +0200

    Question 01

---
+++ G-Net-40-LINQ02/Helpers/StringCaseInsensitiveComparer.cs
@@ -0,0 +1,13 @@
+﻿namespace LINQ.Helpers
+{
+	public class StringCaseInsensitiveComparer : IComparer<string>
+	{
+		public int Compare(string? x, string? y)
+		{
+			//1 x > y
+			//0 == 
+			//-1
+			return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+		}
+	}
+}
--- G-Net-40-LINQ02/Program.cs
+++ G-Net-40-LINQ02/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using static LINQ.DataSources.Source;
+
+#region Question 01
+
+// 1. Get top 3 most expensive products 
+
+//var Result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+
+//foreach (var n in Result)
+//{
+//    Console.WriteLine(n);
+//}
+
+#endregion
+
+
